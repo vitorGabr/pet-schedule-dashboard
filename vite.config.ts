2c7f8168a329bd4adb09e3f9e8a1508db9a9ea23
vitorGabr <macstudios.info@gmail.com>
2025-10-24 15:35:47 -0300

    feat: adicionar nitro como dependência e atualizar configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	server: { port: 3000 },
 	plugins: [
-		tsConfigPaths({ projects: ["./tsconfig.json"] }),
 		tanstackStart(),
+		nitro(),
 		viteReact(),
 		tailwindcss(),
+		tsConfigPaths({ projects: ["./tsconfig.json"] }),
 	],
 });

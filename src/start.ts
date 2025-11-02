89432faffeaaf96404b53274ee612a1831f83f23
vitorGabr <macstudios.info@gmail.com>
2025-11-02 17:19:24 -0300

    remover configuração de authorizedParties do clerkMiddleware

--- src/start.ts
+++ src/start.ts
@@ -2,11 +2,5 @@ import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
 import { createStart } from "@tanstack/react-start";
 
 export const startInstance = createStart(() => {
-	return {
-		requestMiddleware: [
-			clerkMiddleware({
-				authorizedParties: [`${process.env.VITE_DOMAIN_ORIGIN}`],
-			}),
-		],
-	};
+	return { requestMiddleware: [clerkMiddleware()] };
 });

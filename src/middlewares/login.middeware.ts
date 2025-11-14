17ef7af8b1d9e6d74165768de23d0aa3511cad3e
vitorGabr <macstudios.info@gmail.com>
2025-11-13 22:33:35 -0300

    feat: comentar redirecionamento no middleware de login

--- src/middlewares/login.middeware.ts
+++ src/middlewares/login.middeware.ts
@@ -1,10 +1,9 @@
 import { auth } from "@clerk/tanstack-react-start/server";
-import { redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 
 export const loginMiddleware = createServerFn().handler(async () => {
 	const { isAuthenticated } = await auth();
 	if (isAuthenticated) {
-		throw redirect({ to: "/" });
+		//throw redirect({ to: "/" });
 	}
 });

42e0da86ab6397c48af34795584551c924a140a6
vitorGabr <macstudios.info@gmail.com>
2025-11-16 17:19:48 -0300

    feat: refatorar middleware de autenticação e login para melhorar a lógica de redirecionamento

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,30 +1,33 @@
 import { auth, clerkClient } from "@clerk/tanstack-react-start/server";
 import { redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
-import { deleteCookie } from "@tanstack/react-start/server";
 
-export const authMiddleware = createServerFn({ method: "POST" }).handler(async () => {
-  const { isAuthenticated, orgId, sessionId, userId } = await auth();
-  const client = clerkClient();
-  if (!isAuthenticated || !orgId || !userId) {
-    if (sessionId) {
-      await client.sessions.revokeSession(sessionId);
-    }
-    deleteCookie("__session");
-    throw redirect({ to: "/sign-in/$" });
-  }
+export const authMiddleware = createServerFn({ method: "POST" }).handler(
+	async () => {
+		const { isAuthenticated, sessionId, userId } = await auth();
+		const client = clerkClient();
+		if (!isAuthenticated || !userId) {
+			throw redirect({ to: "/sign-in/$" });
+		}
 
-  const [userResult, orgResult] = await Promise.allSettled([
-    client.users.getUser(userId),
-    client.organizations.getOrganization({ organizationId: orgId }),
-  ]);
+		const organizations = await client.users.getOrganizationMembershipList({
+			userId,
+		});
+		if (organizations.totalCount === 0) {
+			if (sessionId) {
+				await client.sessions.revokeSession(sessionId);
+			}
+			throw redirect({ to: "/sign-in/$" });
+		}
 
-  if (userResult.status === "fulfilled" && orgResult.status === "fulfilled") {
-    return {
-      userId: String(userResult.value.publicMetadata?.appUserId),
-      companyId: String(orgResult.value.publicMetadata?.appCompanyId),
-    };
-  }
+		const userResult = await client.users.getUser(userId);
+		if (userResult) {
+			return {
+				userId: String(userResult.publicMetadata?.appUserId),
+				companyId: String(organizations.data[0]?.publicMetadata?.appCompanyId),
+			};
+		}
 
-  throw redirect({ to: "/waiting" });
-});
+		throw redirect({ to: "/waiting" });
+	},
+);
--- src/middlewares/login.middeware.ts
+++ src/middlewares/login.middeware.ts
@@ -1,10 +1,9 @@
-import { redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 import { getCookie } from "@tanstack/react-start/server";
 
 export const loginMiddleware = createServerFn().handler(async () => {
 	const isAuthenticated = getCookie("__session");
 	if (isAuthenticated) {
-		throw redirect({ to: "/" });
+		//throw redirect({ to: "/" });
 	}
 });
